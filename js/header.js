1c5dd9a7f663ca7e0441b66464405f4f8c5b8374
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-06-04 21:52:26 +0200

    added new pages

--- js/header.js
+++ js/header.js
@@ -2,6 +2,10 @@
 let headerNavbar = document.getElementsByClassName("navbar-editing")[0];
 let headerNavbarHeight = headerNavbar.offsetHeight;
 
-// Get text
+// Get socialIcons lenght
+let sosialIcons = document.getElementsByClassName("cosialIcons")[0];
+let sosialIconsHeight = sosialIcons.offsetHeight;
 
+let headerImage = document.getElementsByClassName("header__image")[0];
+headerImage.style.height = `calc(100vh-${headerNavbarHeight}px-${sosialIconsHeight}px)`;
 

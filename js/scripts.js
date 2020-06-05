78c0aac706e148bcbcfe1e4086cd22902ae33294
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-06-05 13:20:24 +0200

    added gallery

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    new WOW().init();
-    $('.progress .progress-bar').css("width",
-        function () {
-            return $(this).attr("aria-valuenow") + "%";
-        }
-    )
-});

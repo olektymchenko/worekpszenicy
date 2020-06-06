bfec51b65602c4a18dcdee648acfa9e37f161c7e
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-06-06 17:43:25 +0200

    added 2 languages and preloader

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+    let preloader = $("#preloader");
+    setTimeout(function () {
+        preloader.remove();
+    });
+
+    let PolLang = $("#PolLanguage");
+    let EngLang = $("#EngLanguage");
+    let RusLang = $("#RusLanguage");
+
+    let website = document.location.href.split('app/')[0];
+
+    EngLang.click(function () {
+        document.location.href = website + "app/en/index.html"
+    });
+    PolLang.click(function () {
+        document.location.href = website + "app/index.html"
+    });
+
+    RusLang.click(function () {
+        document.location.href = website + "app/ru/index.html"
+    })
+
+});
